5b95a8aff211a6ac5b97f1638ed6d7e320687d10
Artem <kibervarrior@gmail.com>
2024-02-28 10:04:25 +0300

    Changed some project structure

--- Client/SettingsValues.py
+++
@@ -1,7 +0,0 @@
-BackgroundColor = (178, 102, 255)
-RegistrationBackgroundColor = (255,188,0)
-RegistrationButtonColor = (178,102,255)
-ScreenWidth = 1920
-ScreenHeight = 1080
-MaxLoginLength = 15
-MaxPasswordLength = 15
