a75e71d423fe442e07b059cb9899964c6fc13a61
gabik <gabidullinkamil1@gmail.com>
2024-03-02 16:52:41 +0300

    PEP8

--- server/db_prepare.py
+++ server/db_prepare.py
@@ -2,10 +2,10 @@ from db_connection import DBConnection
 from logs.loggers import get_logger
 
 if __name__ == "__main__":
-    logger = get_logger(__name__)
+    _LOGGER = get_logger(__name__)
     try:
         db = DBConnection()
         db.drop_and_create_schema()
-        logger.warning("База данных успешно сброшена. Созданы нужные таблицы")
+        _LOGGER.warning("База данных успешно сброшена. Созданы нужные таблицы")
     except Exception as e:
-        logger.error(f"Ошибка при попытке сбросить базу данных. Попробуйте позже\n {e}")
+        _LOGGER.error(f"Ошибка при попытке сбросить базу данных. Попробуйте позже\n {e}")
