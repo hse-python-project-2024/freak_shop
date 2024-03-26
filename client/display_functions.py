030c86312361717a1d5ba4807add493c8df12f31
Artem <kibervarrior@gmail.com>
2024-03-26 13:14:59 +0300

    Card clicnking works now(but discounts are not considered yet) + Added new images for selected cards

--- client/display_functions.py
+++ client/display_functions.py
@@ -3,6 +3,10 @@ from interface_setup import *
 
 class GameBoardView:
     def __init__(self):
+        self.ClickedPlayerCards = [0] * 10
+        self.ClickedPlayerCardsDiscounted = [0] * 10
+        self.ClickedShopCards = [0] * 10
+        self.ClickedShopCardsDiscounted = [0] * 10
         self.ShopImage = pygame.image.load("src/img/Shop_Image.jpg").convert()
         self.CardImages = []
         for i in range(10):
@@ -15,17 +19,33 @@ class GameBoardView:
                     CardName += "Non_Discount_Image_"
                 CardName += str(i + 1)
                 CardName += ".png"
-                self.CardImages[i].append(pygame.transform.scale(pygame.image.load(CardName).convert(),(140, 200)))
+                self.CardImages[i].append(pygame.transform.scale(pygame.image.load(CardName).convert(), (140, 200)))
+        self.CardImagesSelected = []
+        for i in range(10):
+            self.CardImagesSelected.append([])
+            for j in range(2):
+                CardName = "src/img/"
+                if j == 1:
+                    CardName += "Discount_Image_"
+                else:
+                    CardName += "Non_Discount_Image_"
+                CardName += str(i + 1)
+                CardName += "_Selected.png"
+                self.CardImagesSelected[i].append(
+                    pygame.transform.scale(pygame.image.load(CardName).convert(), (140, 200)))
         self.TaskImages = []
         for i in range(12):
             TaskName = "src/img/Task_"
             TaskName += str(i + 1)
             TaskName += ".png"
             self.TaskImages.append(pygame.transform.scale(pygame.image.load(TaskName).convert(), (140, 200)))
-        self.PlayerIcon = pygame.transform.scale(pygame.image.load("src/img/Player_Icon.png").convert_alpha(), (140, 140))
-        self.EndTurnIcon = pygame.transform.scale(pygame.image.load("src/img/End_Turn_Icon.png").convert_alpha(), (250, 250))
-        self.EndTurnIconActivated = pygame.transform.scale(pygame.image.load("src/img/End_Turn_Icon_Activated.png").convert_alpha(),
-                                                           (250, 250))
+        self.PlayerIcon = pygame.transform.scale(pygame.image.load("src/img/Player_Icon.png").convert_alpha(),
+                                                 (140, 140))
+        self.EndTurnIcon = pygame.transform.scale(pygame.image.load("src/img/End_Turn_Icon.png").convert_alpha(),
+                                                  (250, 250))
+        self.EndTurnIconActivated = pygame.transform.scale(
+            pygame.image.load("src/img/End_Turn_Icon_Activated.png").convert_alpha(),
+            (250, 250))
         self.EndTurnText = TextFont.render("Совершить обмен", False, (0, 0, 0))
 
     def display_shop_image(self):
@@ -35,30 +55,57 @@ class GameBoardView:
     def display_player_cards(self, CurrentPlayer):
         for i in range(10):
             DisplayedDiscounted = 0
+            DisplayedDiscountedSelected = 0
+            DisplayedSeclected = 0
             for j in range(CurrentPlayer.CardsInHand[i]):
                 ShowDiscount = False
+                ShowSelected = False
                 if CurrentPlayer.DiscountedCardsInHand[i] > DisplayedDiscounted:
                     DisplayedDiscounted += 1
                     ShowDiscount = True
-                screen.blit(self.CardImages[i][ShowDiscount],
-                            (ScreenWidth * (1 / 30 + i / 12),
-                             ScreenHeight * (6 / 10 + j / 50) + 20))
+                    if self.ClickedPlayerCardsDiscounted[i] > DisplayedDiscountedSelected:
+                        DisplayedDiscountedSelected += 1
+                        ShowSelected = True
+                if self.ClickedPlayerCards[i] > DisplayedSeclected:
+                    DisplayedSeclected += 1
+                    ShowSelected = True
+                if ShowSelected:
+                    screen.blit(self.CardImagesSelected[i][ShowDiscount],
+                                (ScreenWidth * (1 / 30 + i / 12),
+                                 ScreenHeight * (6 / 10 + j / 50) + 20))
+                else:
+                    screen.blit(self.CardImages[i][ShowDiscount],
+                                (ScreenWidth * (1 / 30 + i / 12),
+                                 ScreenHeight * (6 / 10 + j / 50) + 20))
 
     def display_shop_cards(self, CurrentShop):
         for i in range(10):
             DisplayedDiscounted = 0
+            DisplayedDiscountedSelected = 0
+            DisplayedSeclected = 0
             for j in range(CurrentShop.CardsInShop[i]):
-                ShowDiscount = False
-                if CurrentShop.DiscountedCardsInShop[i] > DisplayedDiscounted:
-                    DisplayedDiscounted += 1
-                    ShowDiscount = True
                 WidthAdd = i / 8 + 1 / 100
                 HeightAdd = 1 / 100
                 if i >= 5:
                     WidthAdd = (i - 5) / 8 - 1 / 25 - 1 / 100
                     HeightAdd += 9 / 30
-                screen.blit(self.CardImages[i][ShowDiscount], (ScreenWidth * (2 / 10 + WidthAdd),
-                                                                            ScreenHeight * (HeightAdd + j / 50)))
+                ShowDiscount = False
+                ShowSelected = False
+                if CurrentShop.DiscountedCardsInShop[i] > DisplayedDiscounted:
+                    DisplayedDiscounted += 1
+                    ShowDiscount = True
+                    if self.ClickedShopCardsDiscounted[i] > DisplayedDiscountedSelected:
+                        DisplayedDiscountedSelected += 1
+                        ShowSelected = True
+                if self.ClickedShopCards[i] > DisplayedSeclected:
+                    DisplayedSeclected += 1
+                    ShowSelected = True
+                if ShowSelected:
+                    screen.blit(self.CardImagesSelected[i][ShowDiscount], (ScreenWidth * (2 / 10 + WidthAdd),
+                                                                           ScreenHeight * (HeightAdd + j / 50)))
+                else:
+                    screen.blit(self.CardImages[i][ShowDiscount], (ScreenWidth * (2 / 10 + WidthAdd),
+                                                                   ScreenHeight * (HeightAdd + j / 50)))
 
     def display_player_list(self, Game):
         for i in range(Game.PlayerAmount):
@@ -66,7 +113,7 @@ class GameBoardView:
             PlayerNameText = TextFont.render(Game.PlayersNicknames[i], False, (0, 0, 0))
             screen.blit(PlayerNameText, (ScreenWidth * 4 / 5 + 150, ScreenHeight * i / 8 + 60))
 
-    def display_end_turn_button(self,activated):
+    def display_end_turn_button(self, activated):
         screen.blit(self.EndTurnText, (ScreenWidth * 6 / 7 + 15, ScreenHeight * 3 / 4 - 40))
         if activated:
             screen.blit(self.EndTurnIconActivated,
@@ -80,7 +127,13 @@ class GameBoardView:
         for TaskNumber in Game.Tasks:
             TaskImagesRects[ind] = Rect(ScreenWidth / 50, ScreenHeight * ind / 5 + 10, 140,
                                         200)
-            screen.blit(self.TaskImages[TaskNumber-1], (ScreenWidth / 50,
-                                                                        ScreenHeight * ind / 5 + 10))
+            screen.blit(self.TaskImages[TaskNumber - 1], (ScreenWidth / 50,
+                                                          ScreenHeight * ind / 5 + 10))
             ind += 1
         return TaskImagesRects
+
+    def reset(self):
+        self.ClickedPlayerCards = [0] * 10
+        self.ClickedShopCards = [0] * 10
+        self.ClickedPlayerCardsDiscounted = [0] * 10
+        self.ClickedShopCardsDiscounted = [0] * 10
--- client/gameplay_interface.py
+++ client/gameplay_interface.py
@@ -6,19 +6,17 @@ from interface_setup import *
 class Game:
     def __init__(self, NewGameInfo):
         self.Info = NewGameInfo
+        self.EndTurnButtonRect = Rect(ScreenWidth * 6 / 7, ScreenHeight * 3 / 4, 250, 250)
 
     def StartGame(self):
         # get player and shop info
         Player = PlayerInfo()
         Shop = ShopInfo()
-        ClickedPlayerCards = [0] * 10
-        ClickedPlayerCardsDiscounted = [0] * 10
-        ClickedShopCards = [0] * 10
-        ClickedShopCardsDiscounted = [0] * 10
         TaskImagesRects = list()
         UpdateStatus = 1
         GameBoard = GameBoardView()
         while True:
+            IsMyTurn = True  # Ask server about this
             # get new Player and Shop Info
             NewPlayer = PlayerInfo()
             NewShop = ShopInfo()
@@ -42,13 +40,47 @@ class Game:
                 if event.type == QUIT:
                     sys.exit()
                 if event.type == MOUSEBUTTONDOWN:
-                    MousePosition = pygame.mouse.get_pos()
+                    if IsMyTurn:
+                        MousePosition = pygame.mouse.get_pos()
+                        if self.EndTurnButtonRect.collidepoint(MousePosition):
+                            GameBoard.reset()
+
+                        # Shop card click checks
+                        for i in range(10):
+                            for j in range(Shop.CardsInShop[i] - 1, -1, -1):
+                                WidthAdd = i / 8 + 1 / 100
+                                HeightAdd = 1 / 100
+                                if i >= 5:
+                                    WidthAdd = (i - 5) / 8 - 1 / 25 - 1 / 100
+                                    HeightAdd += 9 / 30
+                                CardRect = Rect(ScreenWidth * (2 / 10 + WidthAdd),
+                                                ScreenHeight * (HeightAdd + j / 50), 140, 200)
+                                if CardRect.collidepoint(MousePosition):
+                                    if GameBoard.ClickedShopCards[i] > j:
+                                        GameBoard.ClickedShopCards[i] -= 1
+                                    else:
+                                        GameBoard.ClickedShopCards[i] += 1
+                                    print(GameBoard.ClickedShopCards)
+                                    break
+
+                        # Hand card click checks
+                        for i in range(10):
+                            for j in range(Player.CardsInHand[i] - 1, -1, -1):
+                                CardRect = Rect(ScreenWidth * (1 / 30 + i / 12),
+                                                ScreenHeight * (6 / 10 + j / 50) + 20, 140, 200)
+                                if CardRect.collidepoint(MousePosition):
+                                    if GameBoard.ClickedPlayerCards[i] > j:
+                                        GameBoard.ClickedPlayerCards[i] -= 1
+                                    else:
+                                        GameBoard.ClickedPlayerCards[i] += 1
+                                    print(GameBoard.ClickedPlayerCards)
+                                    break
 
             pressed_keys = pygame.key.get_pressed()
             if pressed_keys[K_ESCAPE]:
                 sys.exit()
 
-            if Rect(ScreenWidth * 6 / 7, ScreenHeight * 3 / 4, 250, 250).collidepoint(pygame.mouse.get_pos()):
+            if self.EndTurnButtonRect.collidepoint(pygame.mouse.get_pos()):
                 GameBoard.display_end_turn_button(True)
             else:
                 GameBoard.display_end_turn_button(False)
