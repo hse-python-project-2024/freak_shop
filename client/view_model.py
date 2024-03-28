0427c9f7ceb6c204766899c62bc7f9c585441f13
Габидуллин Камиль Маратович <kmgabidullin@edu.hse.ru>
2024-03-28 19:18:32 +0300

    add logic

--- client/view_model.py
+++ client/view_model.py
@@ -1,8 +1,8 @@
 import threading
 import time
 from facade import ClientRequests
-
 import enum
+from logs.logger import get_logger
 
 
 class ViewWindows(enum.Enum):
@@ -18,18 +18,24 @@ class ViewWindows(enum.Enum):
 
 class ViewModel:
     def __init__(self):
+        self._LOGGER = get_logger(__name__)
         self.req = ClientRequests()
         self.window = ViewWindows.initial_menu
         self.info_window = None
         self.user_name = None
         self.user_id = None
+        self.user_login = None
         self.game_id = None
         self.card = []
+        self._LOGGER.info("View model is initialized correctly")
 
-    def put_info_window(self, info: str):
-        self.info_window = info
-        time.sleep(2)
+    def put_and_sleep(self, _info: str, _time: float = 2) -> None:
+        self.info_window = _info
+        time.sleep(_time)
         self.info_window = None
+
+    def put_info_window(self, _info: str, _time: float = 2) -> None:
+        threading.Thread(target=self.put_and_sleep, args=(_info, _time), daemon=True).start()
 
     def go_to_main_menu_window(self):
         self.card = []
@@ -51,18 +57,21 @@ class ViewModel:
 
     def login_user(self, _user_login: str, _user_password: str):
         response = self.req.login_user(_user_login=_user_login, _password=_user_password)
-        if response.status.is_done:
-            self.user_id = response.id
-            self.user_name = response.name
+        if response.status == 0:
+            self.user_id = response.user_info.id
+            self.user_name = response.user_info.name
+            self.user_login = response.user_info.login
             self.go_to_main_menu_window()
         else:
-            self.put_info_window(response.status.info)
+            self.put_info_window(str(response.status))
 
     def register_user(self, _user_login: str, _user_name: str, _user_password1: str, _user_password2: str):
         response = self.req.register_user(_user_login=_user_login, _user_name=_user_name, _password1=_user_password1,
                                           _password2=_user_password2)
-        if response.is_done:
-            self.put_info_window(response.info)
+        if response.status == 0:
+            self.put_info_window("Пользователь успешно добавлен")
+        else:
+            self.put_info_window(str(response.status))
 
     def run(self):
         threading.Thread(target=self.current_status, args=(0.1), daemon=True).start()
