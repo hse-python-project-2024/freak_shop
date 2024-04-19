35d30d03013b3cc2d7a8fa476fedb48650c9a4ce
ArtemCh101 <112023266+ArtemCh101@users.noreply.github.com>
2024-04-19 13:10:29 +0300

    Recommiting Gabik commit

--- client/view_model.py
+++ client/view_model.py
@@ -21,7 +21,7 @@ class Languages(enum.Enum):
 
 
 class ViewModel:
-    def __init__(self, language=Languages.russian):
+    def __init__(self, language=Languages.russian):  # нужно добавить поле готовности игрока
         self.req = ClientRequests()
         self.window = ViewWindows.initial_menu
         self.info_window = None
@@ -32,7 +32,13 @@ class ViewModel:
         self.card = []
         self.language = language
 
+    def reset_all_info(self):
+        self.user_id, self.user_name, self.game_id, self.user_login = None, None, None, None
+        self.card = []
+
     def put_and_sleep(self, _info: str, _time: float = 2) -> None:
+        while self.info_window is not None:
+            time.sleep(0.1)
         self.info_window = _info
         time.sleep(_time)
         self.info_window = None
@@ -51,19 +57,18 @@ class ViewModel:
         self.window = ViewWindows.login
 
     def go_to_registration_window(self):
-        self.user_id, self.user_name, self.game_id = None, None, None
+        self.reset_all_info()
         self.window = ViewWindows.registration
 
     def go_to_initial_window(self):
-        self.user_id, self.user_name, self.game_id = None, None, None
+        self.reset_all_info()
         self.window = ViewWindows.initial_menu
 
-    def start_game(self):
-        self.user_id, self.user_name, self.game_id = None, None, None
+    def go_to_game_menu(self):
         self.window = ViewWindows.game
 
     def sign_out(self):
-        self.user_id, self.user_name = None, None
+        self.reset_all_info()
         self.window = ViewWindows.initial_menu
 
     def login_user(self, _user_login: str, _user_password: str):
@@ -81,12 +86,13 @@ class ViewModel:
                                           _password2=_user_password2)
         if response.status == 0:
             self.put_info_window("Пользователь успешно добавлен")
-            self.go_to_initial_window()
+            self.window = ViewWindows.initial_menu
         else:
             self.put_info_window(str(response.status))
 
     def change_language(self, new_language):
         self.language = new_language
+
     def run(self):
         threading.Thread(target=self.current_status, args=(0.1), daemon=True).start()
         threading.Thread(target=self.all_cards, args=(0.2), daemon=True).start()
