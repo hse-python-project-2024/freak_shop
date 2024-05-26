f2fe76d6232cee7b403d9e7dd38b8608aa5071f0
Габидуллин Камиль Маратович <kmgabidullin@edu.hse.ru>
2024-05-26 14:01:53 +0300

    fix tests

--- client/tests/check_model_fields.py
+++ client/tests/check_model_fields.py
@@ -7,13 +7,11 @@ class ModelFields(enum.Enum):
     user_name = 3
     user_id = 4
     user_login = 5
-    user_readiness = 6
     game_id = 7
     my_card = 8
     shop_card = 9
     goals = 10
     language = 11
-    users_in_session = 12
     whose_move = 13
 
 
@@ -33,9 +31,6 @@ def check_fields(model1, model2, not_take: list[ModelFields]):
     if ModelFields.user_login not in not_take:
         assert model1.user_login == model2.user_login
 
-    if ModelFields.user_readiness not in not_take:
-        assert model1.user_readiness == model2.user_readiness
-
     if ModelFields.game_id not in not_take:
         assert model1.game_id == model2.game_id
 
@@ -53,11 +48,5 @@ def check_fields(model1, model2, not_take: list[ModelFields]):
     if ModelFields.language not in not_take:
         assert model1.language == model2.language
 
-    if ModelFields.users_in_session not in not_take:
-        for key in model1.users_in_session:
-            assert key in model2.users_in_session.keys()
-            assert model1.users_in_session[key] == model2.users_in_session[key]
-
     if ModelFields.whose_move not in not_take:
         assert model1.whose_move == model2.whose_move
-
